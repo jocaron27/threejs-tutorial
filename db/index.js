f843491cf0c1d4832e7634d10d9779d875e5ede8
Geoff Bass <geoff.c.bass@gmail.com>
2017-09-01 15:54:57 -0400

    remove didSync and update webpack

--- db/index.js
+++ db/index.js
@@ -5,40 +5,15 @@ const Sequelize = require('sequelize');
 const pkg = require('../package.json');
 
 const name = process.env.DATABASE_NAME || pkg.name;
-const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${pkg.name}`;
+const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${name}`;
 
 console.log(chalk.yellow(`Opening database connection to ${connectionString}`));
 
 // create the database instance that can be used in other database files
-const db = module.exports = new Sequelize(connectionString, {
-  logging: debug, // export DEBUG=sql in the environment to get SQL queries 
+module.exports = new Sequelize(connectionString, {
+  logging: debug, // export DEBUG=sql in the environment to get SQL queries
   native: true    // lets Sequelize know we can use pg-native for ~30% more speed (if you have issues with pg-native feel free to take this out and work it back in later when we have time to help)
 });
 
 // run our models file (makes all associations for our Sequelize objects)
 require('./models')
-
-// sync the db, creating it if necessary
-function sync(force=false, retries=0, maxRetries=5) {
-  return db.sync({force})
-  .then(ok => console.log(`Synced models to db ${connectionString}`))
-  .catch(fail => {
-    // Don't do this auto-create nonsense in prod, or
-    // if we've retried too many times. 
-    if (process.env.NODE_ENV === 'production' || retries > maxRetries) {
-      console.error(chalk.red(`********** database error ***********`))
-      console.error(chalk.red(`    Couldn't connect to ${connectionString}`))
-      console.error()
-      console.error(chalk.red(fail))
-      console.error(chalk.red(`*************************************`))
-      return
-    }
-    // Otherwise, do this autocreate nonsense
-    console.log(`${retries ? `[retry ${retries}]` : ''} Creating database ${name}...`)
-    return new Promise((resolve, reject) =>
-      require('child_process').exec(`createdb "${name}"`, resolve)
-    ).then(() => sync(true, retries + 1))
-  })
-}
-
-db.didSync = sync();
--- db/models/user.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var Sequelize = require('sequelize')
-var db = require('../index.js')
-
-
-module.exports = db.define('user', {
-  name: Sequelize.STRING,
-})
